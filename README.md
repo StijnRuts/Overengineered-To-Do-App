052d308be8065f0cb5d01ff8c32c5acecd7c33e0
Stijn Ruts <git@stijnruts.be>
2026-09-18 21:39:30 +0200

    Haskell

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Overengineered To Do App
 
+The backend is written in [Haskell](https://www.haskell.org).
 The system configuration is [Dendritic](https://github.com/mightyiam/dendritic) [Nix](https://nixos.org) with [flake-file](https://flake-file.denful.dev).
 The code is formatted with [Treefmt](https://treefmt.com).
---
+++ modules/haskell.nix
@@ -0,0 +1,27 @@
+{ lib, ... }: {
+  perSystem = { pkgs, ... }: {
+    make-shells.default =
+      let
+        version = "9.10.3";
+
+        tools = [
+          (pkgs.haskell.packages."ghc${lib.replaceString "." "" version}".ghcWithPackages (_p: [ ]))
+          pkgs.cabal-install
+          pkgs.ghcid
+        ];
+
+        lsp = pkgs.haskell-language-server;
+      in
+      {
+        packages = tools ++ [ lsp ];
+      };
+
+    treefmt = {
+      programs = {
+        ormolu.enable = true;
+        hlint.enable = true;
+        cabal-gild.enable = true;
+      };
+    };
+  };
+}
